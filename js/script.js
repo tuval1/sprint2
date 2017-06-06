52cf2a8be183ec0db6051bf98aab490325c02669
klogpok <kloleg@gmail.com>
2017-06-06 15:43:34 +0300

    up

--- js/script.js
+++ js/script.js
@@ -11,33 +11,41 @@ var gImgs = [
     { id: 4, url: 'assets/img/1.jpg',keyword:[] }
 ];
 
-function createImgList(){
-    var strHtml = '';
-
-    var arr = gImgs.map(function( img ){
-       strHtml += createHex(img);
+function renderImgList(){
+    var el = document.querySelector('.thumb-list');
+    gImgs.forEach(function( img ){
+        el.append(createHex(img))
     });
-    return strHtml;
 }
+
+// function renderItem(selector) {
+//     var el = document.querySelector(selector);
+//     el.append(createHex(gImgs[0]));
+// }
 
 function createHex(img) {
 
-    var template =
-    `
-    <div class="hexagon hexagon2">
-        <div class="hexagon-in1"> 
-            <div class="hexagon-in2" style="background-image: url('${img.url}')"></div>
-        </div>
-    </div>
-    `;
-    return template;
+    var hex = document.createElement('div');
+    hex.classList.add('hexagon');
+    var hex2 = document.createElement('div');
+    hex2.classList.add('hexTop');
+    var hex3 = document.createElement('div');
+    hex3.classList.add('hexBottom');
+    hex.append(hex2);
+    hex.append(hex3);
+    hex.style.backgroundImage = `url(${img.url})`;
+    console.dir(hex.style);
+    return hex;
 }
 
 function render( strHtml, selector ){
-    var el       = document.querySelector(selector);
-    console.log(el);
-    el.innerHTML = strHtml;
+    // var el       = document.querySelector(selector);
+    // el.innerHTML = strHtml;
+    var elems = document.querySelectorAll('.hexagon');
+    console.log(elems);
+    elems[0].style.backgroundImage = gImgs[0].url;
 }
 
-
-render( createImgList(), '.thumb-list' );
+renderImgList();
+var el = document.querySelector('.team-hex');
+el.append(createHex(gImgs[0]));
