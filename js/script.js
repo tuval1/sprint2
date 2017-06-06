e93587f373f0876159c258aa0909fb446d2a2d26
klogpok <kloleg@gmail.com>
2017-06-06 12:10:53 +0300

    added hex

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
+"use strict";
+
+
 var gImgs = [
-    { id: 1, url: 'images/',keyword:[] },
-    { id: 2, url: 'images/',keyword:[] },
-    { id: 3, url: 'images/',keyword:[] },
-    { id: 4, url: 'images/',keyword:[] }
+    { id: 1, url: 'assets/img/1.jpg',keyword:[] }
+    // { id: 2, url: '1.jpg',keyword:[] },
+    // { id: 3, url: '1.jpg',keyword:[] },
+    // { id: 4, url: '1.jpg',keyword:[] }
 ];
 
 function createImgList(){
     var strHtml = '';
 
-    gImgs.map(function( img ){
-        strHtml += `<a href="#">Hi<img src="${img.url}"></a>`;
+    var arr = gImgs.map(function( img ){
+       strHtml += createHex(img);
     });
+    return strHtml;
+}
 
-    console.log(strHtml);
-    return strHtml; 
-         
+function createHex(img) {
+
+    var template =
+    `
+    <div class="hexagon hexagon2">
+        <div class="hexagon-in1"> 
+            <div class="hexagon-in2" style="background-image: url('${img.url}')"></div>
+        </div>
+    </div>
+    `;
+    return template;
 }
 
 function render( strHtml, selector ){
     var el       = document.querySelector(selector);
+    console.log(el);
     el.innerHTML = strHtml;
 }
 
 
-// render( createImgList(), '.container' );
+render( createImgList(), '.thumb-list' );
